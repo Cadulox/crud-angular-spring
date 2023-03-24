22e9fd28379c24c748f7a109a0cdcbbbf3af568f
Cadulox <engenharia.lourenco@gmail.com>
2023-03-23 23:02:15 -0300

    feat: create form field with buttons

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -1,23 +1,21 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppMaterialModule } from '../shared/app-material/app-material.module';
 import { SharedModule } from '../shared/shared.module';
+import { CourseFormComponent } from './course-form/course-form.component';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-import { CourseFormComponent } from './course-form/course-form.component';
-
 
 @NgModule({
-  declarations: [
-    CoursesComponent,
-    CourseFormComponent
-  ],
+  declarations: [CoursesComponent, CourseFormComponent],
   imports: [
     CommonModule,
     CoursesRoutingModule,
     AppMaterialModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule,
   ],
 })
 export class CoursesModule {}
