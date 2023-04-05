49099f494f4d5b6b65e25c0057e199ad217a9cb3
Cadulox <engenharia.lourenco@gmail.com>
2023-04-04 23:34:20 -0300

    chore: update spring version

--- crud-spring/src/main/java/com/cadulox/model/Course.java
+++ crud-spring/src/main/java/com/cadulox/model/Course.java
@@ -2,15 +2,21 @@ package com.cadulox.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inativo' WHERE id = ?")
+// Annotation do Hibernate para soft delete sem alterar o código da Controller
+@Where(clause = "status = 'Ativo'")
+// Annotation do Hibernate pra quando executar um SELECT colocar a condição de trazer objetos Ativos apenas
 public class Course {
 
     @Id
@@ -29,4 +35,10 @@ public class Course {
     @Pattern(regexp = "Back-end|Front-end")
     @Column(length = 10, nullable = false)
     private String category;
+
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(length = 10, nullable = false)
+    private String status = "Ativo";
 }
